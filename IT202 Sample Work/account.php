05409eb91532aff83a859f2f34eb390c8868fac6
Brian A. Orozco <bao7@njit.edu>
2019-02-14 20:24:04 -0500

    Create account.php

---
+++ IT202 Sample Work/account.php
@@ -0,0 +1,7 @@
+
+<?php
+$hostname = "sql.njit.edu" ; // or "sql2.njit.edu"
+$username = "bao7" ; //replace with your personal db name
+$project = "bao7" ; //same as above
+$password = "*insert db pass*" ; //replace with your db password . NOT your ucid password!
+ ?>

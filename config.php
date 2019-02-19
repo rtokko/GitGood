2c52b4cb48fb58d153340fe8957027820ff204f9
cv59 <35509855+cv59@users.noreply.github.com>
2019-02-19 18:41:04 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,12 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
+//Datebase Server
 define('DB_SERVER', 10.0.0.4);
 define('DB_USERID, 'userid');
 define('DB_USERNAME', 'username');
 define('DB_PASSWORD', 'password');
 define('DB_EMAIL' 'email');
  
-/* Attempt to connect to MySQL database */
+/* Connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERID, DB_USERNAME, DB_PASSWORD, DB_EMAIL);
  
 // Check connection

279af62ea6cbd20b2f32bda78a4465c76399fffc
cv59 <cv59@njit.edu>
2019-02-21 16:23:51 -0500

    login with test php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-<<<<<<< HEAD
+
 //include("function.php");
 
 $db = new mysqli('127.0.0.1','testuser','12345678','testdb');
@@ -20,37 +20,19 @@ if ($db->errno != 0)
 	echo __FILE__.':'.__LINE__.":error: " .$db->error.PHP_EOL;
 	exit(0);
 }
-?>
-=======
 
-error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);  
-ini_set('display_errors' , 1);
-
-include (  "config.php"     ) ;
-
-$db = mysqli_connect($hostname,$username, $password ,$project);
-if (mysqli_connect_errno())
-  {
-	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	  exit();
-  }
-print "<br>Successfully connected to MySQL.<br>";
-mysqli_select_db( $db, $project ); 
-?> 
->>>>>>> 388345ee45be02117df68b93f5c6fbbad4bd935a
 <!DOCTYPE HTML>
 <HTML>
 
 	<h2>Git Good Login</h2>
-<<<<<<< HEAD
+
 	
 	<form method="post" action="login.php">
 		
-=======
+
 
 	<form method="post" action="test.php">
 
->>>>>>> 388345ee45be02117df68b93f5c6fbbad4bd935a
 		Username:<br>
 		<input type="text" name="username" id="username">
 		<br><br>
@@ -61,10 +43,10 @@ mysqli_select_db( $db, $project );
 
 		<input type="submit"><a href="D:/GitGood/user_creation.html"><input type="button" value="new user"></a>
 
-<<<<<<< HEAD
+
 </form>
-=======
+
 	</form>
->>>>>>> 388345ee45be02117df68b93f5c6fbbad4bd935a
+
 
 </HTML>

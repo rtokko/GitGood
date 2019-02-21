41a2679758a3f23098ca7f2fded47cc1612b434f
cv59 <cv59@njit.edu>
2019-02-21 11:09:58 -0500

    testdb

--- test.php
+++ test.php
@@ -1,19 +1,15 @@
 #!/usr/bin/php
 <?php
-error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
-ini_set('display_errors', 1);
-
-include("config.php");
-
-$db = mysql_connect($hostname, $username, $password) or die("cannot connect");
-mysql_select_db($db, $db_name) or die("cannot select DB");
+include ("sqlconnect.php");
+global $db;
 $user = $_POST['username'];
 $pass = $_POST['password'];
-$user = mysql_real_escape_string($user);
-$pass = mysql_real_escape_string($pass);
-$sql = "select *from  $table_name where password = '$pass' and name = '$user'";
-$result = mysql_query($sql,$db);
-$count = mysql_num_rows($result);
+$user = mysqli_real_escape_string($db,$user);
+$pass = mysqli_real_escape_string($db,$pass);
+$sql = "select * from testtable where password = '$pass' and name = '$user'";
+$result = mysqli_query($db,$sql);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$count = mysqli_num_rows($result);
 if ($count == 1)
 {
 	echo "Login Success :)";
@@ -22,4 +18,5 @@ else
 {
 	echo "Authentication Failed :(";
 }
+mysqli_close($db);
 ?>

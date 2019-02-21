00b6370aa47060c0a5d4cf6fe6fbb3811c6226ad
cv59 <cv59@njit.edu>
2019-02-21 16:10:38 -0500

    sql test

--- test.php
+++ test.php
@@ -1,22 +1,32 @@
-#!/usr/bin/php
 <?php
-include ("sqlconnect.php");
-global $db;
-$user = $_POST['username'];
-$pass = $_POST['password'];
-$user = mysqli_real_escape_string($db,$user);
-$pass = mysqli_real_escape_string($db,$pass);
-$sql = "select * from testtable where password = '$pass' and name = '$user'";
-$result = mysqli_query($db,$sql);
-$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-$count = mysqli_num_rows($result);
-if ($count == 1)
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+ini_set('display_errors' , 1);
+
+include ("config.php");
+include ("function.php");
+
+$db = mysqli_connect( $hostname, $username, $password, $db_name, $table_name);
+if (mysqli_connect_errno())
 {
-	echo "Login Success :)";
+	echo "Failed to connect to MYSQL : " . mysqli_connect_error();
+	exit();
 }
-else
+print "Successfully connected to MYSQL";
+mysqli_select_db($db, $db_name);
+
+$username = getdata("username");
+$password = getdata("password");
+
+if (! auth ($username, $password))
 {
-	echo "Authentication Failed :(";
+	exit("bad");
 }
+
+$name = $_GET["username"];
+print "$name";
+
 mysqli_close($db);
+exit ("Interaction completed.");
+
 ?>

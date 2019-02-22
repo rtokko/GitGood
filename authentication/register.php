73b61ed2aa29e0e50a7f78a2b1e83744571ab789
cv59 <35509855+cv59@users.noreply.github.com>
2019-02-22 00:16:42 -0500

    Create register.php
    register user

---
+++ authentication/register.php
@@ -0,0 +1,66 @@
+<?php 
+	session_start();
+	// connect to database
+	$db = mysqli_connect("localhost", "testuser", "12345678", "testdb");
+	if (isset($_POST['register_btn'])) {
+		$username = $_POST['username'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		$password2 = $_POST['password2'];
+		if ($password == $password2) {
+			// create user
+//$password = md5($password); //hash password before storing for security purposes
+			$sql = "INSERT INTO users(username, email, password) VALUES('$username', '$email', '$password')";
+			mysqli_query($db, $sql);
+			$_SESSION['message'] = "You are now logged in";
+			$_SESSION['username'] = $username;
+			header("location: home.php"); //redirect to home page
+		}else{
+			$_SESSION['message'] = "The two passwords do not match";
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+</head>
+<body>
+<div class="header"> 
+	<h1>Register</h1>
+</div>
+
+<?php
+	if (isset($_SESSION['message'])) {
+		echo "<div id='error_msg'>".$_SESSION['message']."</div>";
+		unset($_SESSION['message']);
+	}
+?>
+
+<form method="post" action="register.php">
+	<table>
+		<tr>
+			<td>Username:</td>
+			<td><input type="text" name="username" class="textInput"></td>
+		</tr>
+		<tr>
+			<td>Email:</td>
+			<td><input type="email" name="email" class="textInput"></td>
+		</tr>
+		<tr>
+			<td>Password:</td>
+			<td><input type="password" name="password" class="textInput"></td>
+		</tr>
+		<tr>
+			<td>Password again:</td>
+			<td><input type="password" name="password2" class="textInput"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" name="register_btn" value="Register"></td>
+		</tr>
+	</table>
+</form>
+</body>
+</html>

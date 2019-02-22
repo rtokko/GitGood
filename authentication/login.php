540f49335209ef15b83ef4fc8e6019640d17c4e4
cv59 <35509855+cv59@users.noreply.github.com>
2019-02-22 00:18:08 -0500

    Create login.php
    user login page

---
+++ authentication/login.php
@@ -0,0 +1,60 @@
+<?php 
+	session_start();
+	// connect to database
+	$db = mysqli_connect("sql2.njit.edu", "cv59", "christyv1", "cv59");
+	if (isset($_POST['login_btn'])) {
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		//$password = md5($password); // remember we hashed password before storing last time
+		$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+		$result = mysqli_query($db, $sql);
+		if (mysqli_num_rows($result) == 1) {
+			$_SESSION['message'] = "You are now logged in";
+			$_SESSION['username'] = $username;
+			header("location: home.php"); //redirect to home page
+		}else{
+			$_SESSION['message'] = "Username/password combination incorrect";
+		}
+	}
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register & Login</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="header"> 
+	<h1>Register & Login</h1>
+</div>
+<?php
+	if (isset($_SESSION['message'])) {
+		echo "<div id='error_msg'>".$_SESSION['message']."</div>";
+		unset($_SESSION['message']);
+	}
+?>
+
+
+<form method="post" action="login.php">
+	<table>
+		<tr>
+			<td>Username:</td>
+			<td><input type="text" name="username" class="textInput"></td>
+		</tr>
+
+		<tr>
+			<td>Password:</td>
+			<td><input type="password" name="password" class="textInput"></td>
+		</tr>
+
+		<tr>
+			<td></td>
+			<td><input type="submit" name="login_btn" value="Login"></td>
+		</tr>
+	</table>
+</form>
+</body>
+</html>

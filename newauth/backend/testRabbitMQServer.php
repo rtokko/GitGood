a0e03bd5e385fa1853bf0b8f378285151ad9ce10
cv59 <35509855+cv59@users.noreply.github.com>
2019-02-27 22:10:46 -0500

    Update testRabbitMQServer.php

--- newauth/backend/testRabbitMQServer.php
+++ newauth/backend/testRabbitMQServer.php
@@ -13,20 +13,22 @@ function doLogin($username,$password)
     //return false if not valid
 
 	//code modded from our login.php file
-	if (isset($_POST['login_btn'])) {
-		$username = $_POST['username'];
-		$password = $_POST['password'];
+	//if (isset($_POST['index']) 
+		//$username = $_POST['username'];
+		//$password = $_POST['password'];
 		//$password = md5($password); // remember we hashed password before storing last time
 		$sql = "SELECT * FROM test WHERE username='$username' AND password='$password'";
 		$result = mysqli_query($db, $sql);
 		if (mysqli_num_rows($result) == 1) {
 			$_SESSION['message'] = "You are now logged in";
 			$_SESSION['username'] = $username;
-			header("location: home.php"); //redirect to home page
+			return true;
+			//header("location: home.php"); //redirect to home page
 		}else{
 			$_SESSION['message'] = "Username/password combination incorrect";
+			return false;
 		}
-	}
+	//}
 
     
 }
@@ -36,25 +38,27 @@ function doRegistration($username, $email, $password) {
 
 	include('account.php');
 	//code modded from our register.php file
-	if (isset($_POST['register_btn'])) {
-		$username = $_POST['username'];
-		$email = $_POST['email'];
-		$password = $_POST['password'];
-		$password2 = $_POST['password2'];
-		if ($password == $password2) {
+	//if (isset($_POST['registration'])) 
+		//$username = $_POST['username'];
+		//$email = $_POST['email'];
+		//$password = $_POST['password'];
+		//$password2 = $_POST['password2'];
+		$sql = "INSERT INTO test(username, email, password) VALUES('$username', '$email', '$password')";
+		($t = mysqli_query($db, $sql)) or die(mysqli_error($db)); //changed this line in order to error check		
+		if ($t == true) {
 			// create user
 			//$password = md5($password); //hash password before storing for security purposes
-			$sql = "INSERT INTO test(username, email, password) VALUES('$username', '$email', '$password')";
-			($t = mysqli_query($db, $sql)) or die(mysqli_error($db)); //changed this line in order to error check
-			$_SESSION['message'] = "You are now logged in";
-			$_SESSION['username'] = $username;
+			//$_SESSION['message'] = "You are now logged in";
+			//$_SESSION['username'] = $username;
 			//header("location: home.php"); //redirect to home page
+			echo "Registration Successful";
 			return true;
 		}else{
-			$_SESSION['message'] = "The two passwords do not match";
+			//$_SESSION['message'] = "The two passwords do not match";
+			echo "Invalid Registration";
 			return false;
 		}
-	}
+	//}
 
 
 
@@ -79,7 +83,7 @@ function requestProcessor($request)
     case "validate_session":
       return doValidate($request['sessionId']);
     //new code in switch statement
-    case "register":
+    case "registration":
       return doRegistration($request['username'], $request['email'], $request['password']);
   }
   return array("returnCode" => '0', 'message'=>"Server received request and processed");
@@ -92,4 +96,5 @@ $server->process_requests('requestProcessor');
 echo "testRabbitMQServer END".PHP_EOL;
 exit();
 ?>
+
 

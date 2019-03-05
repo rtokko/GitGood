7850b4527df45697c32e2c3b818ed4cfeb689f2c
rtokko <43188000+rtokko@users.noreply.github.com>
2019-03-05 10:18:39 -0500

    Delete account.php

--- account.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$hostname = "10.0.0.7" ; // or "sql2.njit.edu"
-$username = "admin" ; //replace with your personal db name
-$project = "gitgood" ; //Name of DB
-$password = "12345" ; //replace with your db password . NOT your ucid password!
-
-?>

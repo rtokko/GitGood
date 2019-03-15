ac3e96b04620572c45fa1cdcfaba9b0fe3a50e81
kkp58 <47511925+kkp58@users.noreply.github.com>
2019-03-14 20:35:46 -0400

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', '38629938826-ona4lmujuf7qc0rj3fibflk0i06mnoph.apps.googleusercontent.com');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', 'FCOBfN4uNBLnOQoXasnH_TSW');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', '');
+
+?>

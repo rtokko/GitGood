744b78dfbf18cc6603b6a98cfc018d3ba6ccc3c1
Brian A. Orozco <bao7@njit.edu>
2019-03-20 12:34:08 -0400

    Create sgg.js
    calling smash.gg api (must be run with node.js)

---
+++ apitest/sgg.js
@@ -0,0 +1,46 @@
+const { request, GraphQLClient } = require('graphql-request')
+const fs = require ('fs')
+
+async function main() {
+  const endpoint = 'https://api.smash.gg/gql/alpha'
+
+const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+      Authorization: 'Bearer ac111b13f58a75b9210eb4db995f99cd',
+    },
+  })
+
+  const query = /* GraphQL */ `
+    query TournamentsByState($perPage: Int, $state: String!) {
+  tournaments(query: {
+    perPage: $perPage
+    filter: {
+      addrState: $state
+    }
+  }) {
+    nodes {
+      id
+      name
+      addrState
+    }
+  }
+},
+  `
+
+  const variables = { 
+  "state": "NJ",
+  "perPage": 5 
+}
+	
+
+  const data = await graphQLClient.request(query, variables)
+  console.log(JSON.stringify(data, undefined, 2))
+  /*fs.writeFile('Output.txt', JSON.stringify(data, undefined, 2), (err) => { 
+      
+    // In case of a error throw err. 
+    if (err) throw err; 
+}) */
+ 
+}
+
+main().catch(error => console.error(error))

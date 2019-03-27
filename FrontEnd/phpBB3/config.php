979b2d7d087740adf4fad949612ea8a826626a7f
rtokko <43188000+rtokko@users.noreply.github.com>
2019-03-26 21:13:49 -0400

    Add files via upload

---
+++ FrontEnd/phpBB3/config.php
@@ -0,0 +1,17 @@
+<?php
+// phpBB 3.2.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'phpbb\\db\\driver\\mysqli';
+$dbhost = '10.0.0.5';
+$dbport = '';
+$dbname = 'gitgood';
+$dbuser = 'admin';
+$dbpasswd = '12345';
+$table_prefix = 'phpbb_1';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
+// @define('PHPBB_DISPLAY_LOAD_TIME', true);
+@define('PHPBB_ENVIRONMENT', 'production');
+// @define('DEBUG_CONTAINER', true);

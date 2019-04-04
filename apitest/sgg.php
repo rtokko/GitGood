3a0032b0caceedfaeffecffeb27cd0ac28b797eb
Brian A. Orozco <bao7@njit.edu>
2019-04-03 20:19:31 -0400

    Update sgg.php

--- apitest/sgg.php
+++ apitest/sgg.php
@@ -1,14 +1,44 @@
 <!DOCTYPE html>
 <html>
+
 <body>
 
-<h1>up tourn</h1>
-
+<h1>Upcoming Tournaments</h1>
+<style>
+ul {
+  background: #D63232;
+  padding: 10px;
+  list-style: none
+}
+ul li {
+  background: #cce5ff;
+  margin: 5px;
+}
+</style>
 <?php
-$text=shell_exec('./sgg.sh');
+session_start();
+//making sure sessions are set to make rmqclient run as intended
+unset($_SESSION["index"]);
+unset($_SESSION["registration"]);
+$_SESSION["api"] = true;
+include('testRabbitMQClient.php');
+
+shell_exec('./sgg.sh');
 //print "$text";
+$text = file_get_contents("output.txt");
+$groups        = explode("}", $text);
+
+echo "<ul>";
+foreach ($groups as $group) {
+	$group = substr($group, 2);
+	echo "<li>";
+
+	echo $group;
+	echo "</li><br>";
+}
+echo"</ul>";
 ?>
-<object width="5000" height="5000" data="output.txt"></object>
+
 
 </body>
 </html>

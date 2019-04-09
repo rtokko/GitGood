eb475df7dc08f6db8086f7ec62763d1dd28bc4c9
Brian A. Orozco <bao7@njit.edu>
2019-04-09 11:36:14 -0400

    Add files via upload

---
+++ Backend/logs/error.php
@@ -0,0 +1,7 @@
+<?php
+ini_set('display_errors',1); //displays errors to html page
+ini_set('log_errors', 1); //log errors
+ini_set('error_log', dirname(__FILE__) . '/log.txt'); //transfers the error messages to log.txt file
+error_reporting(E_ALL); //reports all errors
+
+?>
